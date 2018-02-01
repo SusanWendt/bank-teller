c4177dc0afd42d60ce0d3a060fd5ad8a6fb45e8f
SusanWendt <wendt.37@osu.edu>
2018-01-31 20:44:21 -0500

    deposit and withdraw test added

--- src/main/java/bankteller/BankAccount.java
+++ src/main/java/bankteller/BankAccount.java
@@ -3,9 +3,9 @@ package bankteller;
 public class BankAccount {
 
 	// [x] 3 instance variables String accountNum, String type, double balance
-	// [] note the accountNum will be a unique identifier
-	// [] create Constructor that handles the instance data in order above
-	// [] create appropriate methods needed for bank transactions
+	// [X] note the accountNum will be a unique identifier
+	// [X] create Constructor that handles the instance data in order above
+	// [X] create appropriate methods needed for bank transactions
 
 	String accountNum;
 	String type;
@@ -32,6 +32,23 @@ public class BankAccount {
 
 	public double getBalance() {
 		return balance;
+	}
+
+	public void deposit(double amountToDeposit) {
+		if (amountToDeposit > 0) {
+			balance = balance + amountToDeposit;
+		} else {
+			balance = balance + 0;
+		}
+	}
+
+	public void withdraw(int amountToWithdraw) {
+		if (amountToWithdraw > 0) {
+			balance = balance - amountToWithdraw;
+		} else {
+			balance = balance - 0;
+		}
+
 	}
 
 }
--- src/test/java/bankteller/BankAccountTest.java
+++ src/test/java/bankteller/BankAccountTest.java
@@ -7,28 +7,43 @@ public class BankAccountTest {
 
 	@Test
 	public void shouldInitiateBankAccount() {
-		BankAccount underTest = new BankAccount(); 
+		BankAccount underTest = new BankAccount();
 		Assert.assertNotNull(underTest);
 	}
-	
+
 	@Test
 	public void shouldGetBankAccountNum() {
 		BankAccount underTest = new BankAccount("1234", "savings", 100.00);
 		String result = underTest.getAccountNum();
 		Assert.assertEquals("1234", result);
 	}
+
 	@Test
 	public void shouldGetType() {
 		BankAccount underTest = new BankAccount("1234", "savings", 100.00);
 		String result = underTest.getType();
 		Assert.assertEquals("savings", result);
 	}
+
 	@Test
 	public void shouldGetBalance() {
 		BankAccount underTest = new BankAccount("1234", "savings", 100.00);
 		double result = underTest.getBalance();
 		Assert.assertEquals(100, result, 0.01);
 	}
-//	@Test
-	
+
+	@Test
+	public void shouldDepositAmount() {
+		BankAccount underTest = new BankAccount("1234", "savings", 100.00);
+		underTest.deposit(50);
+		double result = underTest.getBalance();
+		Assert.assertEquals(150, result, 0.01);
+	}
+	@Test 
+	public void shouldWithdrawAmount() {
+		BankAccount underTest = new BankAccount("1234", "savings", 100.00);
+		underTest.withdraw(25);
+		double result = underTest.getBalance();
+		Assert.assertEquals(75, result, 0.01);
+	}
 }
